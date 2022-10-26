29f439c051848bee558161aee5ba3f64c820ed7f
Saleh Masum <salehmasum@gmail.com>
2022-10-26 22:28:51 +1100

    Onboarding added

---
+++ MilkyWay/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,78 @@
+//
+//  OnboardingViewController.swift
+//  MilkyWay
+//
+//  Created by Saleh Masum on 26/10/2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    let heroImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.contentMode = .scaleAspectFit
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        return imageview
+    }()
+    
+    let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.numberOfLines = 0
+        return titleLabel
+    }()
+    
+    let heroImageName: String
+    let titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented - not using storyboards")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func setup() {
+        view.backgroundColor = .systemBackground
+        heroImageView.image = UIImage(named: heroImageName)
+        titleLabel.text = titleText
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(heroImageView)
+        stackView.addArrangedSubview(titleLabel)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+}
--- MilkyWay/SceneDelegate.swift
+++ MilkyWay/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        let navController = UINavigationController(rootViewController: LoginViewController())
-        window.rootViewController = navController
+        window.rootViewController = OnboardingContainerViewController()
         self.window = window
     }
 
